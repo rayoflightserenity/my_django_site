b6ac0a14736619b45ad6c4b601bc07d343aaf6d7
rayoflightserenity <ray.of.light.serenity@gmail.com>
2026-09-17 23:16:40 +0300

    створила програму main додала html css налаштувала URL

--- my_django_site/settings.py
+++ my_django_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-va&ezvz-**sx&@am3l%&!v%uv#+%5#*5!!dipj_n6ykmr@1t2f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'nocturnelegacy.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
