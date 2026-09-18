c75fd7c73a13a9b5935f8c8d827b7e2adfaeee4a
rayoflightserenity <ray.of.light.serenity@gmail.com>
2026-09-18 09:36:46 +0300

    Fix static settings

--- my_django_site/settings.py
+++ my_django_site/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'main' / 'static',
+]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
